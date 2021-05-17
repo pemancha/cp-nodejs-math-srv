6192253e9a96fd18e19cb9a93b7aaf7bc924777d
pemancha <paulgalbert@gmail.com>
2021-05-17 13:44:33 +0800

    add interval

--- index.js
+++ index.js
@@ -21,3 +21,20 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Listening on port 8080...')
 })
+
+
+<script>
+     var time = new Date().getTime();
+     $(document.body).bind("mousemove keypress", function(e) {
+         time = new Date().getTime();
+     });
+
+     function refresh() {
+         if(new Date().getTime() - time >= 60000) 
+             window.location.reload(true);
+         else 
+             setTimeout(refresh, 10000);
+     }
+
+     setTimeout(refresh, 10000);
+</script>
